1a05a5d1564f45712ce1227cf14db23b85230f0f
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2016-05-25 02:21:50 +0000

    move test added in 0.12.6 to after a period is run because a prerequisite demands it in order to test the correct line of code

--- test/index.js
+++ test/index.js
@@ -215,10 +215,6 @@ describe('simulation with values [10,9,8] all below costs [20,40]', function(){
 	it('.periodDuration should be 600 (default, 10 min = 600 sec virtual duration)', function(){
 	    S.periodDuration.should.equal(600);
 	});
-	it('.logTrade({totalQ:2}) should throw because of single unit trade requirement', function(){
-	    var logTwoUnitTrade = function(){ S.logTrade({totalQ:2}) };
-	    logTwoUnitTrade.should.throw();
-	});
     });
 
     var tests_for_config_costs_exceed_values = function(state){
@@ -248,6 +244,11 @@ describe('simulation with values [10,9,8] all below costs [20,40]', function(){
 	    state.S.logs.period.data.length.should.be.equal(1);
 	    state.S.logs.period.data.should.deepEqual([[0,0,0,0,0]]);
 	}); 
+	it('.logTrade({totalQ:2}) should throw because of single unit trade requirement', function(){
+	    var logTwoUnitTrade = function(){ state.S.logTrade({totalQ:2}); };
+	    logTwoUnitTrade.should.throw();
+	});
+
     };
 
     describe('runPeriod()', function(){
